5d79e28b74ebd1bf276a4cc639048480cc2630af
robotoroboter <robotoroboter@gmail.com>
2025-05-24 14:36:46 +0200

    Added "find missing number" algorithm (again with git add)

--- src/ArrayMissingNumber.java
+++ src/ArrayMissingNumber.java
@@ -1,2 +1,20 @@
+// Find missing number in an Array
+// Given array of length = n-1, storing distinct (=one after another) integers in range (1..n), missing one number,
+// find the missing number
+
 public class ArrayMissingNumber {
+    public static int findMissingNumber(int[] arr){
+        int n = arr.length + 1;
+        int[] frequencyHashMap = new int[n+1];
+        for (int number : arr){
+            frequencyHashMap[number]++;
+            //System.out.println("number: " + number + " exists in array and added to hashmap. Hashmap: ");
+        }
+        for (int i=1 ; i<frequencyHashMap.length; i++){
+            if (frequencyHashMap[i] == 0){
+                return i;
+            }
+        }
+        return 0;   //should never happen but compiler requires
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[] args) {
-
         //Kadane
-        Kadane kadane = new Kadane();
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(Kadane.maxSubarraySum(arr));
+        int[] arr1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Kadane's algorithm:\n Given array: " + Arrays.toString(arr1) + ", the subarray with max sum of elements, has sum = " + Kadane.maxSubarraySum(arr1));
+        int[] arr2 = {8, 2, 4, 5, 3, 7, 1};
+        System.out.println("Array Missing Number: Given array: " + Arrays.toString(arr2) + ", the missing element = " + ArrayMissingNumber.findMissingNumber(arr2));
     }
 }
